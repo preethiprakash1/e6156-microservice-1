aded4bd68daee50e12683d05cf564c8f307b93a6
preethiprakash1 <pp2769@columbia.edu>
2023-09-19 15:51:41 -0400

    Changed students file path.

--- resources/students.py
+++ resources/students.py
@@ -8,7 +8,7 @@ class StudentsResource:
     #
 
     students_file = \
-        "/Users/preethiprakash/Documents/2023-2024/CloudComputing/MyProjects/e6156-microservice-1/resources/old-students.json"
+        "resources/old-students.json"
 
     def __init__(self):
         self.students = None
